5eae452bec62c686eab955adb052d98ad89c9aaa
Lucas Simas <simas.lucas@hotmail.com>
2019-04-11 02:08:57 -0300

    Docker & MySQL installed & Python & Redis w/conn

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
-From flask import Flask
+from flask import Flask
+import redis
+
+r = redis.Redis(host='redis', port=6379, db=0)
+r.set('foo', 'bar')
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return r.get('foo')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
